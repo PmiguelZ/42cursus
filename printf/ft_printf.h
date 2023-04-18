b03fef16494ae08e02ab7e2957e2ef4f58a854d8
Pablo Miguel Alvarez Torres <paalvare@car15s2.42barcelona.com>
2023-04-18 13:05:55 +0200

    printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paalvare <paalvare@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 20:20:09 by paalvare          #+#    #+#             */
+/*   Updated: 2023/04/18 09:48:45 by paalvare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_printf(const char *format, ...);
+
+#endif
+
